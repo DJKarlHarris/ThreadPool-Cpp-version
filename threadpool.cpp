8d33fba11b1fee28e25513c83683ad3e11dbcf52
djkarlharris <1149338863@qq.com>
2023-02-16 14:27:11 +0800

    完善头文件件

---
+++ threadpool.cpp
@@ -0,0 +1,33 @@
+#include <threadpool.h>
+
+//任务结构体
+typedef struct Task {
+    void (*function)(void* arg);
+    void* arg;
+};
+
+//线程池结构体
+typedef struct ThreadPool {
+
+    Task* taskQueue;  //任务队列
+    int queueCapacity;  //队列最大容量
+    int queueSize;  //队列任务个数
+    int queueFront; //队头指针
+    int queueRear; //队尾指针
+
+    pthread_t* worderID;  //工作线程id数组
+    pthread_t managerID; //管理线程id
+
+    int minNum;  //最小线程数
+    int maxNum;  //最大线程数
+    int buzyNum;  //忙线程数
+    int liveNum;  //存活线程数
+    int exitNum;  //销毁线程数
+
+    int shutdown;  //线程池是否工作，1为工作，2为销毁
+
+    pthread_mutex_t mutexPool;  //线程池锁
+    pthread_mutex_t mutexBuzyNum;  //忙线程变量锁
+    pthread_cond_t notFull;  //判断是否为满
+    pthread_cond_t notEmpty;  //判断是否为空
+}
